26f7e94bede23e21b94b1f5d7828422dcc9a79a8
danieldotnl <danieldotnl@outlook.com>
2024-03-04 09:50:38 +0000

    Add test for counting when condition entities were unavailable

--- tests/integration/test_condition_template.py
+++ tests/integration/test_condition_template.py
@@ -1,8 +1,9 @@
 """Test counter meter flow."""
 
-from pytest_homeassistant_custom_component.common import MockConfigEntry
 from homeassistant.core import HomeAssistant
-from custom_components.measureit.const import DOMAIN
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from custom_components.measureit.const import DOMAIN, SensorState
 from tests import setup_with_mock_config, unload_with_mock_config
 
 COUNTER_ENTRY = MockConfigEntry(
@@ -134,6 +135,56 @@ async def test_only_counting_when_condition_met_start_false(hass: HomeAssistant)
     await hass.async_block_till_done()
 
     hass.states.async_set("sensor.test_counter", "11")
+    await hass.async_block_till_done()
+
+    hass.states.async_set("sensor.test_counter", "8")
+    await hass.async_block_till_done()
+
+    for entity_id in expected_sensors:
+        state = hass.states.get(entity_id)
+        assert state
+        assert state.state == "1"
+
+    await unload_with_mock_config(hass, COUNTER_ENTRY)
+
+async def test_continue_after_condition_entity_unavailable(
+    hass: HomeAssistant,
+):
+    """Test counter_meter should be counting when condition becomes True."""
+    # counter_template = "{{ states('sensor.test_counter') | float % 2 == 0 }}"
+
+    hass.states.async_set("sensor.test_counter", "4")
+    #hass.states.async_set("switch.test_switch", "on")
+    await hass.async_block_till_done()
+    assert hass.states.get("sensor.test_counter").state == "4"
+    #assert hass.states.get("switch.test_switch").state == "on"
+    await setup_with_mock_config(hass, COUNTER_ENTRY)
+
+    expected_sensors = ["sensor.test_hour"]
+    state = hass.states.get("sensor.test_hour")
+    assert state.state == "0"
+    assert state.attributes.get("status") == SensorState.WAITING_FOR_CONDITION
+
+    hass.states.async_set("sensor.test_counter", "6")
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.test_hour")
+    assert state.state == "0" # because the condition is not met
+
+    hass.states.async_set("switch.test_switch", "on")
+    await hass.async_block_till_done()
+    state = hass.states.get("sensor.test_hour")
+    assert state.attributes.get("status") == SensorState.MEASURING
+
+    hass.states.async_set("sensor.test_counter", "9")
+    await hass.async_block_till_done()
+    hass.states.async_set("sensor.test_counter", "10")
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.test_hour")
+    assert state.state == "1"
+
+    hass.states.async_set("switch.test_switch", "off")
     await hass.async_block_till_done()
 
     hass.states.async_set("sensor.test_counter", "8")
